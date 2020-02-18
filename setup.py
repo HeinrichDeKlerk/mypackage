9dee113c46822ead69826103f3dc1b21516f17e4
HeinrichDeKlerk <59819239+HeinrichDeKlerk@users.noreply.github.com>
2020-02-18 12:14:37 +0200

    testing branch changes

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version='0.1',
     packages=find_packages(exclude=['tests*']),
     license='MIT',
-    description='EDSA example python package',
+    description='EDSA example python package testing',
     long_description=open('README.md').read(),
     install_requires=['numpy'],
     url='https://github.com/<username>/<package-name>',
